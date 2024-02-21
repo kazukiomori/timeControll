4e9b8696d291d049255d8f8ef46caa6e1cbfcf4c
kazukiomori <kazuki1110170501@gmail.com>
2024-02-21 22:24:17 +0900

    Initial Commit

---
+++ TimeControll/TimeControllApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeControllApp.swift
+//  TimeControll
+//
+//  Created by Kazuki Omori on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeControllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
