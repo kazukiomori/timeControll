1d49f06acef622f5aad05a2437e52b808ff54ea8
kazukiomori <kazuki1110170501@gmail.com>
2024-03-01 23:50:10 +0900

    [Add]HomeViewを追加

---
+++ TimeControll/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  TimeControll
+//
+//  Created by Kazuki Omori on 2024/03/01.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
